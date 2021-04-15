99d7ff612e27649db55516c047b10cb5700f612e
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-04-16 00:52:23 +0530

    Added: To find the cheapest hotel for given date Ranges

--- src/main/java/com/hotelreservation/HotelReservation.java
+++ src/main/java/com/hotelreservation/HotelReservation.java
@@ -1,16 +1,18 @@
 package com.hotelreservation;
 import com.hotelreservation.com.hotelreservation.Hotel;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 public class HotelReservation {
-   List<Hotel> hotellList = new ArrayList<>();
+    List<Hotel> hotelList = new ArrayList<>();
 
-   public boolean addHotel(){
-       Hotel hotel = new Hotel();
-       boolean isAdded = hotellList.add(hotel);
-       return isAdded;
-   }
+    public boolean addHotel() {
+        Hotel hotel = new Hotel();
+        boolean isAdded = hotelList.add(hotel);
+        return isAdded;
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to the hotel Reservatiomn System");
@@ -18,13 +20,29 @@ public class HotelReservation {
         hotelReservation.addHotel();
     }
 
-    public boolean addHotel(String lakewood,double rates) {
+    public boolean addHotel(String lakewood, double rates) {
         Hotel hotel = new Hotel();
-        boolean isAdded = hotellList.add(hotel);
+        boolean isAdded = hotelList.add(hotel);
         return isAdded;
 
     }
+
+    /* @Description- To find the cheapest rates for the hotel. */
+
+    public String findCheapestHotel(Date[] dates) {
+        ArrayList<Double> cheapRateHotels = new ArrayList<>();
+        for (Hotel hotel : hotelList) {
+            Double rate = 0.0;
+            for (Date date : dates) {
+                rate = hotel.getRate();
+            }
+            cheapRateHotels.add(rate);
+        }
+        Double cheap = cheapRateHotels.stream().min(Comparator.comparing(Double::doubleValue)).orElse(null);
+        int index = cheapRateHotels.indexOf(cheap);
+        return hotelList.get(index).getName();
+
+    }
+
 }
-
-
 
--- src/test/java/com/hotelreservation/HotelReservationTest.java
+++ src/test/java/com/hotelreservation/HotelReservationTest.java
@@ -3,13 +3,41 @@ package com.hotelreservation;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class HotelReservationTest {
 
-   HotelReservation hotelReservation = new HotelReservation();
+    HotelReservation hotelReservation = new HotelReservation();
+
     @Test
     public void givenHotelWhenAddedShouldReturnTrue() {
-        boolean result = hotelReservation.addHotel("Lakewood" ,110);
+        boolean result = hotelReservation.addHotel("Lakewood", 110);
         Assertions.assertTrue(result);
+    }
 
+    @Test
+    public void givenDateRange_whenSearched_shouldReturnCheapestHotelBasedOnWeekdayAndWeekend() {
+       // public void givenBookingDate_ShouldReturn_CheapestHotel ()
+        {
+            SimpleDateFormat df = new SimpleDateFormat("ddMMMMYYYY");
+            String inputDateString[] = {"10sep2020", "11sep2020"};
+
+            Date[] inputDate = null;
+            try {
+                inputDate = new Date[]{df.parse(inputDateString[0]), df.parse(inputDateString[1])};
+            } catch (ParseException e) {
+                System.out.println("Invalid Date Formate");
+                if (!df.format(inputDateString).equals(inputDate)) {
+                    System.out.println("Invalid Date!!");
+                } else
+                    System.out.println("Valid Date");
+            }
+            boolean result = hotelReservation.addHotel("Lakewood", 220.00) &&
+                    hotelReservation.addHotel("Ridgewood", 1200.00);
+            if (result)
+                Assertions.assertEquals("Lakewood", hotelReservation.findCheapestHotel(inputDate));
+        }
     }
 }
